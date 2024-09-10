471d17b1287776f98220c4152f1d446194d27e71
Shishira <shishiraiyar@gmail.com>
2024-09-10 16:46:06 +0530

    Project card first commit

---
+++ src/components/Projectcard.svelte
@@ -0,0 +1,23 @@
+<div class="container">
+    <h2>Book Companion</h2>
+    <p>A library management system built for a charity that provides books to underprivileged children</p>
+
+    <a>Source</a>
+    <a>Demo</a>
+
+</div>
+
+<style>
+    .container {
+        background-color: var(--card-background-color);
+        border: 1px solid var(--card-border-color);
+        padding: 5%;
+        margin: 5px;
+        border-radius: 5px;
+    }
+
+    h2{
+        margin: 0;
+        font-size: 1.3rem;
+    }
+</style>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+    import Projectcard from "../../components/Projectcard.svelte";
+
+
+</script>
+
+<h1>My Projects</h1>
+
+<div class="container">
+    <Projectcard></Projectcard>
+    <Projectcard></Projectcard>
+    <Projectcard></Projectcard>
+    <Projectcard></Projectcard>
+
+</div>
+
+<style>
+    .container {
+        display: grid;
+        grid-template-columns: 1fr;
+        gap: 1rem;
+    }
+
+    @media only screen and (min-width: 650px) {
+        .container {
+            grid-template-columns: 1fr 1fr;
+        }
+    }
+</style>
+
